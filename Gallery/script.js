6cdca380462086c25c0ba9c92760e50b0f43a74e
Merge20 <keshavr7200@gmail.com>
2025-03-27 16:51:36 +0530

    Sara files link kardiye ekdusre se

---
+++ Gallery/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const places = document.querySelector(".places");
+    const toggleButton = document.querySelector(".toggle");
+
+    toggleButton.addEventListener("click", function () {
+        places.classList.toggle("active");
+        console.log("heelo");
+    });
+});
